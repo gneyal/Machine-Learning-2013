ad18014881194c2f5f097e15b50314dcc05b3638
Eyal-Mac <gneyal@gmail.com>
2013-11-24 23:53:52 +0200

    gradient for sigmoid function

--- mlclass-ex4-004/mlclass-ex4/sigmoidGradient.m
+++ mlclass-ex4-004/mlclass-ex4/sigmoidGradient.m
@@ -12,15 +12,11 @@ g = zeros(size(z));
 % Instructions: Compute the gradient of the sigmoid function evaluated at
 %               each value of z (z can be a matrix, vector or scalar).
 
-
-
-
-
-
-
-
-
-
+for i = 1:size(g)(1)
+	for j = 1:size(g)(2)
+		g(i,j) = (1/(1 + e^(-z(i,j)))) * (1 - (1/(1 + e^(-z(i,j)))))
+	endfor
+endfor
 
 
 
