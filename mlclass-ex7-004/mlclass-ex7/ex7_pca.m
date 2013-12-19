d5bfbf59e12e3cdd3a7be8552b900cdf71015254
Eyal-Mac <gneyal@gmail.com>
2013-12-19 19:53:42 +0200

    finished ex7

--- mlclass-ex7-004/mlclass-ex7/ex7_pca.m
+++ mlclass-ex7-004/mlclass-ex7/ex7_pca.m
@@ -79,7 +79,7 @@ pause;
 fprintf('\nDimension reduction on example dataset.\n\n');
 
 %  Plot the normalized dataset (returned from pca)
-plot(X_norm(:, 1), X_norm(:, 2), 'bo');
+plot(X_norm(:, 1), X_norm(:, 2), 'bo');		
 axis([-4 3 -4 3]); axis square
 
 %  Project the data onto K = 1 dimension
